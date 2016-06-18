5de3d3112e3f7ea2cd0879ea21388c0d8d033d14
Frankie <fdsimms@gmail.com>
2016-06-18 14:16:15 -0400

    chore: create eslint stuff

---
+++ .stylelint.config.js
@@ -0,0 +1,33 @@
+/**
+ * http://stylelint.io/user-guide/rules/
+ */
+module.exports = {
+  "ignoreFiles": "./src/css/BootstrapGrid.css",
+  "rules": {
+    "indentation": 2,
+    "max-line-length": 80,
+    "no-eol-whitespace": true,
+    "no-invalid-double-slash-comments": true,
+    "no-missing-eof-newline": true,
+    "color-hex-case": "lower",
+    "color-no-invalid-hex": true,
+    "color-no-named": true,
+    "font-weight-notation": "numeric",
+    "value-no-vendor-prefix": true,
+    "property-no-vendor-prefix": true,
+    "function-comma-space-after": "never",
+    "function-comma-space-before": "never",
+    "function-parentheses-space-inside": "never-single-line",
+    "declaration-colon-space-after": "always"
+    "declaration-colon-space-before": "never",
+    "declaration-block-semicolon-space-before": "never",
+    "declaration-block-no-single-line": true,
+    "selector-combinator-space-after": "always",
+    "selector-combinator-space-before": "always",
+    "block-opening-brace-space-before": "always",
+    "selector-list-comma-newline-after": "always",
+    "selector-list-comma-space-before": "never",
+    "at-rule-no-vendor-prefix": true,
+    "rule-non-nested-empty-line-before": "always"
+  }
+};
