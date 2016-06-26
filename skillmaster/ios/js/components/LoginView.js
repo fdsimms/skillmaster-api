48c4d7e512bc06dfb11619bd44d778dd4ea8e649
Frankie <fdsimms@gmail.com>
2016-06-26 15:55:23 -0400

    feature: link login inputs to state

--- skillmaster/ios/js/components/LoginView.js
+++ skillmaster/ios/js/components/LoginView.js
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   View,
   StyleSheet,
   TextInput,
-  TouchableHighlight
+  TouchableHighlight,
+  ActivityIndicatorIOS
 } from 'react-native';
 
-const LoginView = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.logo}> Skillmaster </Text>
-      <TextInput style={styles.input} placeholder='Username' />
-      <TextInput style={styles.input} placeholder='Password' />
-      <TouchableHighlight>
-        <Text>Submit</Text>
-      </TouchableHighlight>
-    </View>
-  );
-};
+export default class LoginView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { showProgress: false };
+  }
+
+  onLoginPress() {
+    this.setState({ showProgress: true });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}> Skillmaster </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='First Name'
+          onChangeText={(text) => this.setState({ fname: text })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Last Name'
+          onChangeText={(text) => this.setState({ lname: text })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Nickname'
+          onChangeText={(text) => this.setState({ nickname: text })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+          onChangeText={(text) => this.setState({ email: text })}
+        />
+        <TextInput
+          style={styles.input}
+          secureTextEntry
+          placeholder='Password'
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+        <TouchableHighlight onPress={this.onLoginPress.bind(this)}>
+          <Text>Submit</Text>
+        </TouchableHighlight>
+        {this.state.showProgress &&
+          <ActivityIndicatorIOS
+            animating={this.state.showProgress}
+            size='large'
+          />
+        }
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -41,6 +84,3 @@ const styles = StyleSheet.create({
     padding: 4
   }
 });
-
-
-export default LoginView;
