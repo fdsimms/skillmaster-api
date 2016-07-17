70aa6d0a5302ee862f896c8f6f807171d313fa77
Frankie <fdsimms@gmail.com>
2016-07-17 19:56:21 -0400

    refactor: remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   get '/auth/facebook/callback', to: "api/sessions#omniauth_facebook"
 
-  resources :users
-  resource :session, only: [:new, :create, :destroy]
-
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:index, :show, :create, :update]
     resource :session, only: [:create, :destroy, :show]
