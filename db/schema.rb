1c9ad5f6ca329e5f4f509a553f2e311b4c7b4e37
Frankie <fdsimms@gmail.com>
2016-07-24 19:58:12 -0400

    chore: add UID and provider fields to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160724221929) do
+ActiveRecord::Schema.define(version: 20160724225747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,11 +45,14 @@ ActiveRecord::Schema.define(version: 20160724221929) do
     t.string   "session_token",               null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["nickname"], name: "index_users_on_nickname", using: :btree
   add_index "users", ["password_digest"], name: "index_users_on_password_digest", using: :btree
   add_index "users", ["session_token"], name: "index_users_on_session_token", using: :btree
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
 end
