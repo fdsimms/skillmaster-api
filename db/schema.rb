03cb7fa3d0c35f67991dcc1ffa6471bcae675402
Frankie <fdsimms@gmail.com>
2016-07-24 18:13:02 -0400

    feat: migration for sports table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626190738) do
+ActiveRecord::Schema.define(version: 20160724215634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sports", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sports", ["name"], name: "index_sports_on_name", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "nickname",                    null: false
