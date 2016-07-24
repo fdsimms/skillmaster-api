1a35332365f260ff11ea82e27a48f46e89bf22a2
Frankie <fdsimms@gmail.com>
2016-07-24 18:15:38 -0400

    chore: add a user and sport to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake dbseed (or created alongside the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+jacob_francis = User.create({
+  nickname: "Jake",
+  password: "skillmaster1",
+  fname: "Jacob",
+  lname: "Francis",
+  email: "jf@skillmaster.fake",
+  points: 200
+})
+
+Sport.destroy_all
+soccer = Sport.create({name: "soccer"})
