07a3a2c7f5fa73aefe5219cfd2cb38210fc7ae73
Frankie <fdsimms@gmail.com>
2016-07-24 18:14:45 -0400

    feat: controller for Sports

---
+++ app/controllers/api/sports_controller.rb
@@ -0,0 +1,42 @@
+class Api::SportsController < ApplicationController
+  def create
+    @sport = Sport.new(sport_params)
+
+    if @sport.save
+      log_in!(@sport)
+      render :show
+    else
+      render json: @sport.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @sport = Sport.find(params[:id])
+    if @sport.update(sport_params)
+      render :show
+    else
+      render json: @sport.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @sport = Sport.find(params[:id])
+    @sport.destroy!
+    render :show
+  end
+
+  def show
+    @sport = Sport.find(params[:id])
+  end
+
+  def index
+    @sport = Sport.all
+  end
+
+  private
+
+  def sport_params
+    params.require(:sport).permit(:name)
+  end
+
+end
